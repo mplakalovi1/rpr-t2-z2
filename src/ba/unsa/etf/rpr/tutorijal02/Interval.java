040a222638702b666369f391cb97f87c96f5466c
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-02 00:21:15 +0100

    nadam se uradjen zadatak :D

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,52 +1,50 @@
 package ba.unsa.etf.rpr.tutorijal02;
-/*
-isNull koja vraća boolean vrijednost da li je interval null interval ili nije;
-isIn metoda koja prima vrijednost tačke (tipa double) i vraća da li tačka pripada intervalu;
-metodu intersect koja vraća presjek dva intervala: metoda treba biti urađena kao klasična public metoda prima jedan interval) i kao statička metoda (prima dva intervala);
-također treba preklopiti metode klase Object a to su toString i equals, na način koji ima smisla za te metode.
 
-*/
 public class Interval {
-    private double pocetnaTacka,krajnjaTacka;
-    boolean pripadnostPocetne,pripadnostKrajnje;
+    boolean pripadnostPocetne, pripadnostKrajnje;
+    private double pocetnaTacka, krajnjaTacka;
 
-    public Interval(double pocetnaTacka,double krajnjaTacka,boolean pripadnostPocetne,boolean pripadnostKrajnje){
-        if(pocetnaTacka>krajnjaTacka){
-            throw IllegalArgumentException;
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadnostPocetne, boolean pripadnostKrajnje) {
+        if(pocetnaTacka>krajnjaTacka) {
+            throw new IllegalArgumentException();
         }
-        this.pocetnaTacka=pocetnaTacka;
-        this.krajnjaTacka=krajnjaTacka;
-        this.pripadnostPocetne=pripadnostPocetne;
-        this.pripadnostKrajnje=pripadnostKrajnje;
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadnostPocetne = pripadnostPocetne;
+        this.pripadnostKrajnje = pripadnostKrajnje;
     }
 
-    public Interval(){
-        pocetnaTacka=0;
-        krajnjaTacka=0;
-        pripadnostPocetne=false;
-        pripadnostKrajnje=false;
+    public Interval() {
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        pripadnostPocetne = false;
+        pripadnostKrajnje = false;
     }
 
-    public Interval (Interval interval) { //konstruktor koji prima objekat tipa Interval
-        this (interval.pocetnaTacka,interval.krajnjaTacka,interval.pripadnostPocetne,interval.pripadnostKrajnje);
+    public Interval(Interval interval) { //konstruktor koji prima objekat tipa Interval
+        this(interval.pocetnaTacka, interval.krajnjaTacka, interval.pripadnostPocetne, interval.pripadnostKrajnje);
     }
 
+    public static Interval intersect(Interval i, Interval j) {
+        Interval presjek = new Interval(i.intersect(j));
+        return presjek;
+    }
 
     public boolean isNull() {
-        if (pocetnaTacka == 0 && krajnjaTacka == 0 && pripadnostPocetne = false && pripadnostKrajnje = false) {
+        if ((pocetnaTacka == 0 && krajnjaTacka == 0) && (pripadnostPocetne == false && pripadnostKrajnje == false)) {
             return true;
         }
         return false;
     }
 
     public boolean isIn(double tacka) {
-        if(pripadnostPocetne && tacka==this.pocetnaTacka){
+        if (pripadnostPocetne && tacka == this.pocetnaTacka) {
             return true;
         }
-        if(pripadnostKrajnje && tacka==this.krajnjaTacka){
+        if (pripadnostKrajnje && tacka == this.krajnjaTacka) {
             return true;
         }
-        if(tacka>this.pocetnaTacka && tacka<this.krajnjaTacka){
+        if (tacka > this.pocetnaTacka && tacka < this.krajnjaTacka) {
             return true;
         }
         return false;
@@ -54,26 +52,57 @@ public class Interval {
 
     public Interval intersect(Interval interval) {
 
-        if (this.isIn(interval.pocetnaTacka) && this.isIn(interval.krajnjaTacka)) { //slucaj kada interval u potpunosti pripada drugomintervalu;
-            Interval presjek=new Interval(interval);
+        if (this.isIn(interval.pocetnaTacka) && this.isIn(interval.krajnjaTacka)) { //slucaj kada interval u potpunosti pripada drugom intervalu;
+            Interval presjek = new Interval(interval);
             return presjek;
         }
 
         if (this.isIn(interval.pocetnaTacka)) {
-            Interval presjek=new Interval(interval.pocetnaTacka,this.krajnjaTacka, interval.pripadnostPocetne,this.pripadnostKrajnje);
+            Interval presjek = new Interval(interval.pocetnaTacka, this.krajnjaTacka, interval.pripadnostPocetne, this.pripadnostKrajnje);
             return presjek;
         }
 
         if (this.isIn(interval.krajnjaTacka)) {
-            Interval presjek=new Interval(this.pocetnaTacka,interval.krajnjaTacka, this.pripadnostPocetne,interval.pripadnostKrajnje);
+            Interval presjek = new Interval(this.pocetnaTacka, interval.krajnjaTacka, this.pripadnostPocetne, interval.pripadnostKrajnje);
             return presjek;
         }
+
+        Interval presjek = new Interval(); // ako nema presjeka vraca "null" skup;
+        return presjek;
+    }
+
+    public String toString() {
+        if (this.isNull()) {
+            return "()";
+        }
+        String s = new String();
+        if (pripadnostPocetne) {
+            s = "[";
+        } else {
+            s = "(";
+        }
+        s = s + this.pocetnaTacka + "," + this.krajnjaTacka;
+
+        if (pripadnostKrajnje) {
+            s = s + "]";
+        } else {
+            s = s + ")";
+        }
+        return s;
+    }
+
+    public boolean equals(Interval interval){
+        if(this==interval){
+            return true;
+        }
+        if( (this.pocetnaTacka==interval.pocetnaTacka &&  this.krajnjaTacka==interval.krajnjaTacka) && (this.pripadnostPocetne==interval.pripadnostPocetne && this.pripadnostKrajnje==interval.pripadnostKrajnje) ){
+            return true;
+        }
+        return false;
     }
 
 
-
-
-    }
+}
 
 
 
